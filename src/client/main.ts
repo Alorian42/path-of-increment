2944b1598ecd4175b4d1379d41492910312bf8e1
Alorian42 <alorian42@gmail.com>
2023-12-24 17:45:18 +0100

    00 Client - Alorian42

--- src/client/main.ts
+++ src/client/main.ts
@@ -2,4 +2,5 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).mount('#app');
